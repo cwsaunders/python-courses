05527c6332271f59863056a255b06d0afd16989f
Christian Saunders <noname45688@gmail.com>
2020-05-11 21:29:57 -0400

    Create courseworknotes1.py

---
+++ access-web-data/assignment3/courseworknotes1.py
@@ -0,0 +1,17 @@
+# TCP IP Stack
+
+import socket
+
+# Connection to server
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('data.pr4e.org',80))
+
+# Get information
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP:/1.0\r\n\r\n'.encode()
+mysock.send(cmd)
+while True:
+    data = mysock.recv(512)
+    if (len(data) < 1):
+        break
+    print(data.decode())
+mysock.close()
