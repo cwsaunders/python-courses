9d59d436cf81bd4d9e70e5c69412127431097616
Christian Saunders <noname45688@gmail.com>
2020-05-16 21:42:22 -0400

    Update geojsonnotes.py

--- access-web-data/assignment6-json/6-2/geojsonnotes.py
+++ access-web-data/assignment6-json/6-2/geojsonnotes.py
@@ -27,3 +27,10 @@ while True:
     
     # dumping
     print(json.dumps(js,indent=4))
+    
+    
+    lat = js['results'][0]['geometry']['location']['lat']
+    lng = js['results'][0]['geometry']['location']['lng']
+    print('lat',lat,'lng',lng)
+    location = js['results'][0]['formatted_address']
+    print(location)
