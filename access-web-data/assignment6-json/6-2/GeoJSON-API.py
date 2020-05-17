ce3e0ecddcfb1407036131411e489453b13ddd03
Christian Saunders <noname45688@gmail.com>
2020-05-17 04:19:57 -0400

    Update GeoJSON-API.py

--- access-web-data/assignment6-json/6-2/GeoJSON-API.py
+++ access-web-data/assignment6-json/6-2/GeoJSON-API.py
@@ -1,4 +1,8 @@
-# Google API (use of alias API for unlimited testing)
-# Import statements
-import json
-import urllib.request,urllib.parse,urllib.error
+# Use of Google Geolocation API (Through alias)
+
+'''
+Due to using the free version of this course I was not able to attain access to the current version of the assignment. Additionally, this
+was the only assignment that was not available to find the information for online during the course. 
+
+'''
+
